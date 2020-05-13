f33871c5942802f317ea4a33291e06725c7213b2
stevencpttm <56670584+stevencpttm@users.noreply.github.com>
2020-05-13 20:38:46 +0800

    completed start and stop logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,180 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="stopwatch">
+      {{ displayTime }}
+    </div>
+    <div id="stopwatch-controls">
+      <button id="reset-and-lap">
+        Reset
+      </button>
+      <button id="start-and-stop" @click="startAndStopHandler" :class="{ red: (isStarted && !isStopped) }">
+        {{ (isStarted && !isStopped) ? 'Stop' : 'Start' }}
+      </button>
+      <span id="brand">Stopwatch</span>
+    </div>
+    <ul id="stopwatch-records">
+      <li>
+        <span>Lap 1</span>
+        <span>00:01.23</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      displayTime: '00:00.0',
+      timer: null,
+      startTime: null,
+      stopTime: null,
+      stoppedTimeOffset: 0,
+      currentTime: null,
+      isStarted: false,
+      isStopped: false,
+    }
+  },
+  methods: {
+    startAndStopHandler() {
+
+      if (!this.isStarted) {
+        this.startTime = Date.now()
+
+        this.timer = setInterval(() => {
+          this.currentTime = Date.now()
+          let elapsedTime = this.currentTime - this.startTime
+
+          this.displayTime = this.formatTime((elapsedTime / 1000).toFixed(2))
+        }, 10)
+
+        this.isStarted = true
+      } else if (this.isStarted && !this.isStopped) {
+        clearInterval(this.timer)
+        this.stopTime = Date.now()
+        this.isStopped = true
+      } else if (this.isStarted && this.isStopped) {
+        this.stoppedTimeOffset += Date.now() - this.stopTime
+        
+        this.timer = setInterval(() => {
+          this.currentTime = Date.now()
+          let elapsedTime = this.currentTime - this.startTime - this.stoppedTimeOffset
+
+          this.displayTime = this.formatTime((elapsedTime / 1000).toFixed(2))
+        }, 10)
+
+        this.isStopped = false
+      }
+    },
+    formatTime(seconds) {
+      let date = new Date(null)
+      date.setSeconds(seconds)
+      let result = date.toISOString().substr(14, 5)
+
+      return `${result}.${(seconds + '').split('.')[1]}`
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #000;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+}
+
+#stopwatch {
+  flex: 0px 1 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 5rem;
+}
+
+#stopwatch-records {
+  flex: 0px 1 1;
+}
+
+#stopwatch-records {
+  list-style: none;
+  padding: 0;
+  margin: 0 1.2rem;
+  overflow: auto;
+}
+
+#stopwatch-records li {
+  border-bottom: 1px solid #323234;
+  display: flex;
+  padding: .8rem 0;
+}
+
+#stopwatch-records li.green {
+  color: #2ED158;
+}
+
+#stopwatch-records li.red {
+  color: #FF453A;
+}
+
+#stopwatch-records li:first-child {
+  border-top: 1px solid #323234;
+}
+
+#stopwatch-records span {
+  display: block;
+  flex: 0px 1 1;
+}
+
+#stopwatch-records span:last-child {
+  text-align: right;
+}
+
+#stopwatch-controls {
+  flex: 80px 0 0;
+  padding: .2rem 1.2rem;
+  overflow: hidden;
+  position: relative;
+  margin-bottom: 1.5rem;
+}
+
+#brand {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+}
+
+button#reset-and-lap, button#start-and-stop {
+  width: 80px;
+  height: 80px;
+  outline: none;
+  background-color: #333;
+  border: 2px solid #000;
+  border-radius: 100%;
+  color: #fff;
+  font-size: 1rem;
+  box-shadow: 0px 0px 0px 2px #333;
+  float: left;
+}
+
+button#start-and-stop {
+  background-color: #082A12;
+  box-shadow: 0px 0px 0px 2px #082A12;
+  color: #2ED158;
+  float: right;
+}
+
+button#start-and-stop.red {
+  background-color: #320E0B;
+  box-shadow: 0px 0px 0px 2px #320E0B;
+  color: #FF453A;
 }
 </style>
